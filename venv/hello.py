8f3b633edcc8d16c68ea3c0451fe610a5d9bd4b6
JuliyaOwer <ower4enko0909@gmail.com>
2021-11-02 20:39:37 +0800

    Initial commit

---
+++ venv/hello.py
@@ -0,0 +1,77 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+from analis import *
+
+UPLOAD_FOLDER = 'D:\\Example\\name\\'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'czi'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('preprocessed_file')) #redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Начальная страница загрузки</title>
+    <h1>Загрузите новое изображение</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Загрузить>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.route('/processed')
+def preprocessed_file():
+    return '''
+    <!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Анализ изображений</title>
+    </head>
+    <body>
+        <p style="text-align: center">
+        <button> <a href=click>Данные о бактериях</a></button>
+    </p>
+    </body>
+    </html>
+    '''
+
+@app.route('/click')
+def on_click():
+    return redirect(url_for('analisys'))
+
+@app.route('/analisys')
+def analisys():
+    img_files = [file for file in os.listdir('D://Example/name')]
+    for i in range(len(img_files)): segment_index(i, img_files)
+    return '''
+    <!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Анализ изображений</title>
+    </head>
+    <body>
+        <p style="text-align: center">
+            <div> Данные анализа успешно сохранены.</div>
+        </p>
+    </body>
+    </html>
+    '''
