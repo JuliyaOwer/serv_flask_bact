1e92e1c494aa05f094bd46aa62f6c9273bbf4837
JuliyaOwer <ower4enko0909@gmail.com>
2021-11-29 18:42:26 +0800

    main / error in calc

--- venv/hello.py
+++ venv/hello.py
@@ -1,35 +1,160 @@
 import os
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
-from analis import *
-
+import cv2
+import numpy as np
+import pandas as pd
+import cv2
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import czifile
 
 SIMAGE_UPLOAD_FOLDER = 'D:\\Example\\simage\\'
-RIMAGE_FOLDER = ''
+RIMAGE_FOLDER = 'D:\\Example\\rimage\\'
 SDATA_UPLOAD_FOLDER = 'D:\\Example\\sdata\\'
-RDATA_FOLDER = ''
-
+RDATA_FOLDER = 'D:\\Example\\rdata\\'
 
 IMAGE_ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'czi'])
 DATA_ALLOWED_EXTENSIONS = set(['csv', 'xlsx'])
-
 
 app = Flask(__name__)
 app.config['SIMAGE_UPLOAD_FOLDER'] = SIMAGE_UPLOAD_FOLDER
 app.config['SDATA_UPLOAD_FOLDER'] = SDATA_UPLOAD_FOLDER
 
+
+
 def allowed_image(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in IMAGE_ALLOWED_EXTENSIONS
+
 
 def allowed_data(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in DATA_ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+core = np.array([[0, 0, 1, 0, 0],
+                 [0, 1, 1, 1, 0],
+                 [1, 1, 1, 1, 1],
+                 [0, 1, 1, 1, 0],
+                 [0, 0, 1, 0, 0]], dtype=np.uint8)
+
+
+###### CalcFunctions ######
+def stat(data):
+    data_df = pd.DataFrame(data, columns=('MA', 'ma'))
+    data_df.to_csv(RDATA_FOLDER + "/allDetectedObjects.csv", sep=";", index=False)
+
+    data_df = data_df.drop(data_df[data_df['ma'] > 100].index)
+    data_df = data_df.drop(data_df[data_df['MA'] < 10].index)
+    data_df.to_csv(RDATA_FOLDER + "/clear.csv", sep=";", index=False)
+
+    print(data_df)
+    data_df = data_df / 100
+
+    clasterNum = 3
+    k_means = KMeans(init="k-means++", n_clusters=clasterNum, n_init=12)
+    k_means.fit(data_df)
+    labels = k_means.labels_
+
+    data_df['clust'] = labels
+    data_df.to_csv(RDATA_FOLDER + "/clear_norm_clust.csv", sep=";", index=False)
+
+
+def segment_index(index: int):
+    img_files = [file for file in os.listdir(SIMAGE_UPLOAD_FOLDER)]
+    segment_file(img_files[index])
+
+def e_d(image, it):
+    image = cv2.erode(image, core, iterations=it)
+    image = cv2.dilate(image, core, iterations=it)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, core, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_ERODE, core, iterations=1)
+    return image
+
+
+def segment_file(img_file: str):
+    img_path = SIMAGE_UPLOAD_FOLDER + "\\" + img_file
+
+    if img_file.rsplit('.', 1)[1] == "czi":
+        img = czifile.imread(img_path)
+        img = img[0, :, :, :]
+    else:
+        img = cv2.imread(img_path)
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    th = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    th = e_d(th.copy(), 1)
+
+    cnt, hie = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cntImg = th.copy()
+    for contour in cnt:
+        x, y, w, h = cv2.boundingRect(contour)
+
+        if w > img.shape[1] / 2:
+            continue
+
+        else:
+
+            cntImg = cv2.drawContours(cntImg, [cv2.convexHull(contour)], -1, 255, -1)
+
+    cntImg = e_d(cntImg, 2)
+
+    cnt2, hie2 = cv2.findContours(cntImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    finalContours = []
+    for contour in cnt2:
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        center = (int(x), int(y))
+        radius = int(radius)
+
+        circleImg = np.zeros(img.shape, dtype=np.uint8)
+        circleImg = cv2.circle(circleImg, center, radius, 255, -1)
+
+        contourImg = np.zeros(img.shape, dtype=np.uint8)
+        contourImg = cv2.drawContours(contourImg, [contour], -1, 255, -1)
+
+        union_inter = cv2.bitwise_xor(circleImg, contourImg)
+
+        ratio = np.sum(union_inter == 255) / np.sum(circleImg == 255)
+
+        if ratio > 0.55:
+            finalContours.append(contour)
+
+    finalContours = np.asarray(finalContours)
+
+    bacteriaImg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    data = []
+    cent = []
+
+    for bacteria in finalContours:
+        M = cv2.moments(bacteria)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+
+        # bacteriaImg = cv2.circle(bacteriaImg, (cx, cy), 5, (0, 0, 255), -1)
+        cv2.circle(bacteriaImg, (cx, cy), 5, (0, 0, 255), -1)
+        ellipse = cv2.fitEllipse(bacteria)
+        cv2.ellipse(bacteriaImg, ellipse, (0, 255, 0), 2)[2]
+        (x, y), (MA, ma), ellipse = cv2.fitEllipse(bacteria)
+        cent.append([x, y])
+        data.append([MA, ma])
+
+    cv2.imwrite(RIMAGE_FOLDER + "\\" + img_file, bacteriaImg)
+    return data
+
+
+
+
+###########################
+
+
+@app.route('/')
 def main_page():
-    return render_template("func_page.html")
+    return render_template("main_page.html")
+
 
 """
 @app.route('/uploads/<filename>')
@@ -40,7 +165,7 @@ def uploaded_file(filename):
 
 
 ################################
-#Переход по страничкам с главной
+# Переход по страничкам с главной
 ################################
 
 @app.route('/upload_file', methods=['GET', 'POST'])
@@ -53,6 +178,7 @@ def upload_file():
             return redirect(url_for('image_functions'))
     return render_template("upload_image.html")
 
+
 @app.route('/upload_dataframe', methods=['GET', 'POST'])
 def upload_dataframe():
     if request.method == 'POST':
@@ -63,30 +189,41 @@ def upload_dataframe():
             return redirect(url_for('data_functions'))
     return render_template("upload_dataframe.html")
 
+
 @app.route('/image_functions')
 def image_functions():
-    return render_template("functions.html")
+    return render_template("image_functions.html")
+
 
 @app.route('/data_functions')
-def data_functions():######### ПОМЕНЯТЬ ОТОБРАЖАЕМЫЙ ШАБЛОН ################
-    return render_template("functions.html")
+def data_functions():
+    return render_template("data_functions.html")
 
 
 @app.route('/full_analis')
 def full_analis():
     return redirect(url_for('analisys'))
 
+
 @app.route('/preprocess_image')
 def preprocess_image():
-    return "preprocess_iamge"
+    return "preprocess_image"
+
 
 @app.route('/final_results')
 def final_results():
     return "final_results"
+
 
 #########################
 
 
 @app.route('/analis')
 def analisys():
-    return "full_analis"
+    img_files = [file for file in os.listdir(SIMAGE_UPLOAD_FOLDER)]
+    data = pd.DataFrame()
+    for i in range(len(img_files)):
+        data.append(segment_index(i))
+    stat(data)
+    return print(data) #render_template("main_page.html")
+
