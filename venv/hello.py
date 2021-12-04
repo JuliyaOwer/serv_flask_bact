48ec18db92a72e5e7cc07a6bc4e21d41facf517d
JuliyaOwer <ower4enko0909@gmail.com>
2021-12-04 19:08:35 +0800

    first stable image processing version

--- venv/hello.py
+++ venv/hello.py
@@ -1,4 +1,5 @@
 import os
+import zipfile
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 import cv2
@@ -7,12 +8,12 @@ import pandas as pd
 import czifile
 from sklearn.cluster import KMeans
 
-SIMAGE_UPLOAD_FOLDER = 'D:\\Example\\simage\\'
-RIMAGE_FOLDER = 'D:\\Example\\rimage\\'
-SDATA_UPLOAD_FOLDER = 'D:\\Example\\sdata\\'
-RDATA_FOLDER = 'D:\\Example\\rdata\\'
+SIMAGE_UPLOAD_FOLDER = 'D:\\Example\\simage'
+RIMAGE_FOLDER = 'D:\\Example\\rimage'
+SDATA_UPLOAD_FOLDER = 'D:\\Example\\sdata'
+RDATA_FOLDER = 'D:\\Example\\rdata'
 
-img_files = [file for file in os.listdir(SIMAGE_UPLOAD_FOLDER)]
+data_files = [file for file in os.listdir(SDATA_UPLOAD_FOLDER)]
 
 IMAGE_ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'czi'])
 DATA_ALLOWED_EXTENSIONS = set(['csv', 'xlsx'])
@@ -20,6 +21,8 @@ DATA_ALLOWED_EXTENSIONS = set(['csv', 'xlsx'])
 app = Flask(__name__)
 app.config['SIMAGE_UPLOAD_FOLDER'] = SIMAGE_UPLOAD_FOLDER
 app.config['SDATA_UPLOAD_FOLDER'] = SDATA_UPLOAD_FOLDER
+app.config['RDATA_FOLDER'] = RDATA_FOLDER
+app.config['RIMAGE_FOLDER'] = RIMAGE_FOLDER
 
 
 def allowed_image(filename):
@@ -38,11 +41,31 @@ core = np.array([[0, 0, 1, 0, 0],
                  [0, 1, 1, 1, 0],
                  [0, 0, 1, 0, 0]], dtype=np.uint8)
 
+def stat_culc():
+    data = pd.DataFrame()
+    data = pd.read_csv(RDATA_FOLDER + "\\clear.csv", sep=";")
 
-###### CalcFunctions ######
+    result = pd.DataFrame(index=['min_MA', 'min_ma', 'max_MA', 'max_ma', 'mean_MA', 'mean_ma', 'DX_MA', 'DX_ma',
+                                 'symmetry_MA', 'symmetry_ma', 'general_sample_size'])
 
-def segment_index(index: int):
-    segment_file(img_files[index])
+    result['data'] = result.index
+    result = result.reset_index(drop=True)
+
+    result["result"] = data['MA'].min(), \
+                    data['ma'].min(), \
+                    data['MA'].max(), \
+                    data['ma'].max(), \
+                    data['MA'].mean(), \
+                    data['ma'].mean(), \
+                    data['MA'].std(), \
+                    data['ma'].std(), \
+                    data['MA'].skew(), \
+                    data['ma'].skew(), \
+                    len(data.index)
+    result.to_csv(RDATA_FOLDER + '\\stat_culc.csv', sep=';', index=False)
+
+def segment_index(dir, index: int):
+    segment_file(dir[index])
 
 def e_d(image, it):
     image = cv2.erode(image, core, iterations=it)
@@ -51,19 +74,16 @@ def e_d(image, it):
     image = cv2.morphologyEx(image, cv2.MORPH_ERODE, core, iterations=1)
     return image
 
+def read_image(path, file):
+    if file.rsplit('.', 1)[1] == "czi":
+        img = czifile.imread(path)
+        return img[0, :, :, :]
+    else:
+        return cv2.imread(path)
 
 def segment_file(img_file: str):
     img_path = SIMAGE_UPLOAD_FOLDER + "\\" + img_file
-
-    img = []
-
-    if img_file.rsplit('.',1)[1] == "czi":
-        img = czifile.imread(img_path)
-        img = img[0,:,:,:]
-    else:
-        img = cv2.imread(img_path)
-
-    img = cv2.imread(img_path)
+    img = read_image(img_path, img_file)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     th = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
@@ -115,21 +135,22 @@ def segment_file(img_file: str):
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
 
-        # bacteriaImg = cv2.circle(bacteriaImg, (cx, cy), 5, (0, 0, 255), -1)
         cv2.circle(bacteriaImg, (cx, cy), 5, (0, 0, 255), -1)
         ellipse = cv2.fitEllipse(bacteria)
         cv2.ellipse(bacteriaImg, ellipse, (0, 255, 0), 2)[2]
         (x, y), (MA, ma), ellipse = cv2.fitEllipse(bacteria)
         data.append([MA, ma])
 
-    cv2.imwrite(RIMAGE_FOLDER + "\\" + img_file.rsplit('.', 1) [0] + ".jpeg", bacteriaImg)
+    cv2.imwrite(RIMAGE_FOLDER + "\\" + img_file.rsplit('.', 1)[0] + ".jpeg", bacteriaImg)
 
     data_df = pd.DataFrame(data, columns=('MA', 'ma'))
-    data_df.to_csv(RDATA_FOLDER + "/allDetectedObjects.csv", sep=";", index=False)
+    data_df_m = data_df
+    data_df_m.to_csv(RDATA_FOLDER + "/allDetectedObjects.csv", sep=";", index=False)
 
     data_df = data_df.drop(data_df[data_df['ma'] > 100].index)
     data_df = data_df.drop(data_df[data_df['MA'] < 10].index)
-    data_df.to_csv(RDATA_FOLDER + "/clear.csv", sep=";", index=False)
+    data_df_m = data_df
+    data_df_m.to_csv(RDATA_FOLDER + "/clear.csv", sep=";", index=False)
 
     data_df = data_df / 100
 
@@ -138,32 +159,26 @@ def segment_file(img_file: str):
     k_means.fit(data_df)
     labels = k_means.labels_
 
-    data_df['clust'] = labels
-    data_df.to_csv(RDATA_FOLDER + "/clear_norm_clust.csv", sep=";", index=False)
-
-
-###########################
+    data_df_m = data_df
+    data_df_m['clust'] = labels
+    data_df_m.to_csv(RDATA_FOLDER + "/clear_norm_clust.csv", sep=";", index=False)
+    stat_culc()
 
 
 @app.route('/')
 def main_page():
     return render_template("main_page.html")
 
-
-"""
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-"""
-
-
-################################
-# Переход по страничкам с главной
-################################
+def clear_folder(folder):
+    files = [file for file in os.listdir(folder)]
+    for file in files:
+        os.remove(folder + '\\' + file)
 
 @app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
+    clear_folder(SIMAGE_UPLOAD_FOLDER)
+    clear_folder(RIMAGE_FOLDER)
+    clear_folder(RDATA_FOLDER)
     if request.method == 'POST' and 'files' in request.files:
         for file in request.files.getlist('files'):
             if file and allowed_image(file.filename):
@@ -171,15 +186,6 @@ def upload_file():
                 file.save(os.path.join(app.config['SIMAGE_UPLOAD_FOLDER'], filename))
         return redirect(url_for('image_functions'))
     return render_template("upload_image.html")
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        for f in request.files.getlist('photo'):
-            f.save(os.path.join(app.config['UPLOAD_PATH'], f.filename))
-        return 'Upload completed.'
-    return render_template('upload.html')
-
 
 
 @app.route('/upload_dataframe', methods=['GET', 'POST'])
@@ -197,6 +203,9 @@ def upload_dataframe():
 def image_functions():
     return render_template("image_functions.html")
 
+@app.route('/download_files')
+def download_files():
+    return render_template("download_files.html")
 
 @app.route('/data_functions')
 def data_functions():
@@ -218,12 +227,43 @@ def final_results():
     return "final_results"
 
 
-#########################
-
 @app.route('/analis')
 def analisys():
-    print(img_files)
+    img_files = [file for file in os.listdir(SIMAGE_UPLOAD_FOLDER)]
     for i in range(len(img_files)):
-        segment_index(i)
-    return render_template("main_page.html")
+        segment_index(img_files, i)
+    return render_template("download_files.html")
+
+
+@app.route('/csv_only')
+def csv_only():
+    old_wd = os.getcwd()
+    os.chdir(RDATA_FOLDER)
+    res_files = [file for file in os.listdir(RDATA_FOLDER)]
+    for file in res_files:
+        zipFile = zipfile.ZipFile('data.zip', 'a', zipfile.ZIP_DEFLATED)
+        zipFile.write(RDATA_FOLDER+'\\'+file)
+        zipFile.close()
+        if os.path.exists(RDATA_FOLDER+'\\'+file):
+            os.remove(RDATA_FOLDER+'\\'+file)
+    os.chdir(old_wd)
+    return download(RDATA_FOLDER, 'data.zip')
+
+@app.route('/image_only')
+def image_only():
+    old_wd = os.getcwd()
+    os.chdir(RIMAGE_FOLDER)
+    res_files = [file for file in os.listdir(RIMAGE_FOLDER)]
+    for file in res_files:
+        zipFile = zipfile.ZipFile('images.zip', 'a', zipfile.ZIP_DEFLATED)
+        zipFile.write(RIMAGE_FOLDER+'\\'+file)
+        zipFile.close()
+        if os.path.exists(RIMAGE_FOLDER+'\\'+file):
+            os.remove(RIMAGE_FOLDER+'\\'+file)
+    os.chdir(old_wd)
+    return download(RIMAGE_FOLDER, 'images.zip')
+
+@app.route('/download/<filename>')
+def download(dir, filename):
+    return send_from_directory(dir, filename)
 
